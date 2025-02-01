1298297c2082b1c8261d96eeea693141d5b15bb3
Le Van Thang <blackyugih5@gmail.com>
2025-02-01 12:53:22 +0700

    sua lai QuanLyTaiKhoan

--- QuanLyCuaHangSach/frmQuanLyTaiKhoan.Designer.cs
+++ QuanLyCuaHangSach/frmQuanLyTaiKhoan.Designer.cs
@@ -28,98 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtSoDienThoai = new System.Windows.Forms.TextBox();
+            this.cboChucVu = new System.Windows.Forms.ComboBox();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.btnTimKiem = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
-            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(338, 26);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(109, 97);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(100, 22);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(109, 26);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(100, 22);
+            this.txtTenDangNhap.TabIndex = 2;
+            // 
+            // txtSoDienThoai
+            // 
+            this.txtSoDienThoai.Location = new System.Drawing.Point(338, 97);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.Size = new System.Drawing.Size(100, 22);
+            this.txtSoDienThoai.TabIndex = 3;
+            // 
+            // cboChucVu
+            // 
+            this.cboChucVu.FormattingEnabled = true;
+            this.cboChucVu.Location = new System.Drawing.Point(561, 60);
+            this.cboChucVu.Name = "cboChucVu";
+            this.cboChucVu.Size = new System.Drawing.Size(121, 24);
+            this.cboChucVu.TabIndex = 4;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(-2, 154);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(801, 236);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(-2, 427);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(116, 48);
+            this.btnThem.TabIndex = 6;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(432, 427);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(116, 48);
+            this.btnLuu.TabIndex = 7;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(291, 427);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(116, 48);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(137, 427);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(116, 48);
+            this.btnSua.TabIndex = 9;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(683, 427);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(116, 48);
+            this.btnTimKiem.TabIndex = 10;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(561, 427);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(116, 48);
+            this.btnHuy.TabIndex = 11;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Số điện thoại";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Email";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(259, 112);
+            this.label5.Location = new System.Drawing.Point(501, 63);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(250, 35);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Danh sách tài khoản";
-            // 
-            // dataGridView
-            // 
-            this.dataGridView.BackgroundColor = System.Drawing.Color.Ivory;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(-1, 150);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowHeadersWidth = 51;
-            this.dataGridView.RowTemplate.Height = 24;
-            this.dataGridView.Size = new System.Drawing.Size(800, 360);
-            this.dataGridView.TabIndex = 3;
-            // 
-            // btnTimKiem
-            // 
-            this.btnTimKiem.Location = new System.Drawing.Point(12, 12);
-            this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(84, 27);
-            this.btnTimKiem.TabIndex = 10;
-            this.btnTimKiem.Text = "Tìm Kiếm";
-            this.btnTimKiem.UseVisualStyleBackColor = true;
-            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
-            // 
-            // btnThem
-            // 
-            this.btnThem.Location = new System.Drawing.Point(59, 69);
-            this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(94, 40);
-            this.btnThem.TabIndex = 13;
-            this.btnThem.Text = "Thêm";
-            this.btnThem.UseVisualStyleBackColor = true;
-            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
-            // 
-            // btnXoa
-            // 
-            this.btnXoa.Location = new System.Drawing.Point(606, 69);
-            this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(94, 40);
-            this.btnXoa.TabIndex = 14;
-            this.btnXoa.Text = "Xóa";
-            this.btnXoa.UseVisualStyleBackColor = true;
-            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
-            // 
-            // btnSua
-            // 
-            this.btnSua.Location = new System.Drawing.Point(327, 69);
-            this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(94, 40);
-            this.btnSua.TabIndex = 15;
-            this.btnSua.Text = "Sửa";
-            this.btnSua.UseVisualStyleBackColor = true;
-            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
-            // 
-            // txtTimKiem
-            // 
-            this.txtTimKiem.Location = new System.Drawing.Point(123, 17);
-            this.txtTimKiem.Name = "txtTimKiem";
-            this.txtTimKiem.Size = new System.Drawing.Size(676, 22);
-            this.txtTimKiem.TabIndex = 16;
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Chức vụ";
             // 
             // frmQuanLyTaiKhoan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 496);
-            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnTimKiem);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
             this.Controls.Add(this.btnThem);
-            this.Controls.Add(this.btnTimKiem);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboChucVu);
+            this.Controls.Add(this.txtSoDienThoai);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtEmail);
             this.Name = "frmQuanLyTaiKhoan";
             this.Text = "frmQuanLyTaiKhoan";
+            this.Load += new System.EventHandler(this.frmQuanLyTaiKhoan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -127,12 +226,23 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtSoDienThoai;
+        private System.Windows.Forms.ComboBox cboChucVu;
         private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnLuu;
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.Button btnSua;
-        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- QuanLyCuaHangSach/frmQuanLyTaiKhoan.cs
+++ QuanLyCuaHangSach/frmQuanLyTaiKhoan.cs
@@ -12,126 +12,26 @@ namespace QuanLyCuaHangSach
 {
     public partial class frmQuanLyTaiKhoan : Form
     {
-        public class TaiKhoan
+        void CreateColumnForDataGridView()
         {
-            // Thuộc tính Id (định danh duy nhất cho mỗi tài khoản)
-            public int Id { get; set; }
+            var colTenDangNhap = new DataGridViewTextBoxColumn();
+            var colMatKhau = new DataGridViewTextBoxColumn();
+            var colEmail = new DataGridViewTextBoxColumn();
+            var colSoDienThoai = new DataGridViewTextBoxColumn();
+            var colChucVu = new DataGridViewTextBoxColumn();
 
-            // Thuộc tính Tên đăng nhập
-            public string Ten { get; set; }
+            colTenDangNhap.HeaderText = "Tên đăng nhập";
+            colMatKhau.HeaderText = "Mật khẩu";
+            colEmail.HeaderText = "Email";
+            colSoDienThoai.HeaderText = "Số điện thoại";
+            colChucVu.HeaderText = "Chức vụ";
 
-            // Thuộc tính Email
-            public string Email { get; set; }
-
-            // Thuộc tính Mật khẩu
-            public string MatKhau { get; set; }
-
-            // Thuộc tính Chức vụ
-            public string ChucVu { get; set; }
-        }
-        public List<TaiKhoan> danhSachTaiKhoan = new List<TaiKhoan>();
-        private List<TaiKhoan> danhSachHienTai = new List<TaiKhoan>();
-        internal object chucVu;
-
-        public frmQuanLyTaiKhoan()
-        {
-            InitializeComponent();
-            HienThiDanhSach(danhSachTaiKhoan);
-        }
-        public void HienThiDanhSach(List<frmQuanLyTaiKhoan.TaiKhoan> danhSach)
-        {
-            dataGridView.DataSource = null;
-            dataGridView.DataSource = danhSach.Select(tk => new
-            {
-                Id = tk.Id,
-                Ten = tk.Ten,
-                MatKhau = tk.MatKhau,
-                Email = tk.Email,
-                ChucVu = tk.ChucVu
-            }).ToList();
+            dataGridView.Columns.AddRange(new DataGridViewColumn[] { colEmail, colSoDienThoai, colTenDangNhap, colMatKhau,  colChucVu });
         }
 
-        private void btnThem_Click(object sender, EventArgs e)
+        private void frmQuanLyTaiKhoan_Load(object sender, EventArgs e)
         {
-            frmThemTaiKhoan themForm = new frmThemTaiKhoan(this);
-            themForm.ShowDialog();
-        }
-
-        private void btnSua_Click(object sender, EventArgs e)
-        {
-            // Kiểm tra xem có hàng nào được chọn không
-            if (dataGridView.SelectedRows.Count > 0)
-            {
-                // Lấy hàng được chọn
-                var selectedRow = dataGridView.SelectedRows[0];
-
-                // Lấy dữ liệu từ các ô trong hàng
-                int id = Convert.ToInt32(selectedRow.Cells[0].Value); // Cột Id (chỉ số 0)
-                string ten = selectedRow.Cells[1].Value.ToString();   // Cột Ten (chỉ số 1)
-                string email = selectedRow.Cells[2].Value.ToString(); // Cột Email (chỉ số 2)
-                string matKhau = selectedRow.Cells[3].Value.ToString(); // Cột MatKhau (chỉ số 3)
-                string chucVu = selectedRow.Cells[4].Value.ToString(); // Cột ChucVu (chỉ số 4)
-
-                // Tạo đối tượng TaiKhoan từ dữ liệu lấy được
-                var taiKhoan = new TaiKhoan
-                {
-                    Id = id,
-                    Ten = ten,
-                    Email = email,
-                    MatKhau = matKhau,
-                    ChucVu = chucVu
-                };
-
-                // Mở form sửa và truyền tài khoản được chọn
-                frmSuaTaiKhoan suaForm = new frmSuaTaiKhoan(this, taiKhoan);
-                suaForm.ShowDialog();
-            }
-            else
-            {
-                MessageBox.Show("Vui lòng chọn tài khoản để sửa.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-            if(danhSachHienTai.Count == 0)
-        {
-                MessageBox.Show("Không có dữ liệu để xóa.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            if (dataGridView.SelectedRows.Count > 0)
-            {
-                var selectedRow = dataGridView.SelectedRows[0];
-                var taiKhoan = (TaiKhoan)selectedRow.DataBoundItem;
-
-                // Xóa tài khoản khỏi danh sách gốc
-                danhSachTaiKhoan.Remove(taiKhoan);
-
-                // Cập nhật danh sách hiển thị
-                HienThiDanhSach(danhSachHienTai.Where(tk => tk != taiKhoan).ToList());
-            }
-            else
-            {
-                MessageBox.Show("Vui lòng chọn tài khoản để xóa.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        private void btnTimKiem_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtTimKiem.Text))
-            {
-                MessageBox.Show("Vui lòng nhập email hoặc tài khoản để tìm kiếm.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            string keyword = txtTimKiem.Text.ToLower();
-            var ketQua = danhSachTaiKhoan.Where(tk =>
-                tk.Ten.ToLower().Contains(keyword) ||
-                tk.Email.ToLower().Contains(keyword)).ToList();
-
-            HienThiDanhSach(ketQua); // Hiển thị kết quả tìm kiếm
+            CreateColumnForDataGridView();
         }
     }
 }
